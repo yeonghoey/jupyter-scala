c24eb0bc1ba8de92e038e81f0aeca4337d6789e8
mark <yeonghoey@gmail.com>
2016-03-04 17:59:00 +0900

    Make Dockerfile as template

---
+++ update.py
@@ -0,0 +1,65 @@
+jupyterscala210_install = r'''
+RUN curl -L -o jupyter-scala-2.10 https://git.io/vzhR7 \
+ && chmod +x jupyter-scala-2.10 \
+ && ./jupyter-scala-2.10 \
+ && rm -f jupyter-scala-2.10
+'''.strip()
+
+jupyterscala211_install = r'''
+RUN curl -L -o jupyter-scala https://git.io/vzhRi \
+ && chmod +x jupyter-scala \
+ && ./jupyter-scala \
+ && rm -f jupyter-scala
+'''.strip()
+
+params = {
+        'scala210': {
+            'from'                  : 'java:7',
+            'jupyter_scala_install' : jupyterscala210_install },
+        'scala211': {
+            'from'                  : 'java:8',
+            'jupyter_scala_install' : jupyterscala211_install }
+        }
+
+
+from string import Template
+
+template = Template(r'''
+# Jupyter Notebook with jupyter-scala
+# https://github.com/alexarchambault/jupyter-scala
+
+FROM $from
+
+MAINTAINER Yeongho Kim <yeonghoey@gmail.com>
+
+RUN apt-get update \
+ && apt-get install -y \
+    curl \
+    python3 \
+    python3-pip \
+ && rm -rf /var/lib/apt/lists/*
+
+RUN pip3 install jupyter
+
+${jupyter_scala_install}
+
+# Running Jupyter Notebook in docker has an issue.
+# https://github.com/ipython/ipython/issues/7062
+
+# According to the discussion above,
+# it only works correctly when it starts with a parameterized innter script.
+RUN { echo '#!/bin/bash'; \
+      echo 'jupyter notebook --ip=* --no-browser'; \
+    } > /entrypoint.sh \
+ && chmod +x /entrypoint.sh
+
+
+VOLUME /notebooks
+WORKDIR /notebooks
+EXPOSE 8888
+
+CMD ["/entrypoint.sh"]
+'''.strip())
+
+print template.safe_substitute(params['scala210'])
+print template.safe_substitute(params['scala210'])
